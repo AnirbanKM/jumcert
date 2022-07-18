259b1b4a34590a6e2a4df2d30cf6acd02627acfa
anirban <anirbanshow@kreativemachinez.net>
2022-07-18 17:28:39 +0530

    auth check min app file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,25 +87,27 @@
     @include('frontend.pages.global.chat')
     @include('frontend.pages.global.chatscript')
 
-    @if (Auth::user()->userAccount == null)
-        <div class="user-account-alert-modal show" data-signup-modal>
-            <img src="https://jumcart.previewforclient.com/frontend/img/t-alert-con.svg" alt="">
-            <h5> You need to sign up before going further</h5>
-            <span class="close2" data-close></span>
-        </div>
-        <script>
-            const signupModal = document.querySelector('[data-signup-modal]');
-            const closeSignupModal = document.querySelector('[data-close]');
-            closeSignupModal.addEventListener('click', function(e) {
-                signupModal.classList.toggle('show');
-            });
+    @auth
+        @if (Auth::user()->userAccount == null)
+            <div class="user-account-alert-modal show" data-signup-modal>
+                <img src="https://jumcart.previewforclient.com/frontend/img/t-alert-con.svg" alt="" />
+                <h5> You need to sign up before going further</h5>
+                <span class="close2" data-close></span>
+            </div>
+            <script>
+                const signupModal = document.querySelector('[data-signup-modal]');
+                const closeSignupModal = document.querySelector('[data-close]');
+                closeSignupModal.addEventListener('click', function(e) {
+                    signupModal.classList.toggle('show');
+                });
 
-            setInterval(() => {
-                const signupModal = document.querySelector('.user-account-alert-modal');
-                signupModal.classList.add("show");
-            }, 600000);
-        </script>
-    @endif
+                setInterval(() => {
+                    const signupModal = document.querySelector('.user-account-alert-modal');
+                    signupModal.classList.add("show");
+                }, 600000);
+            </script>
+        @endif
+    @endauth
 
 </body>
 
