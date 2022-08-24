459f6280f8ace6ee9063531f4109d95116f947a8
anirban <anirbanshow@kreativemachinez.net>
2022-08-25 02:03:00 +0530

    user selling transaction

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Frontend\VideoUpload;
+use App\Models\PrivateVideo;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function test()
     {
-        return Auth::user();
+        $user_id = auth()->user()->id;
+
+        $owner_videos = VideoUpload::join('private_videos', 'video_uploads.id', '=', 'private_videos.video_id')
+            ->where('video_uploads.user_id', $user_id)
+            ->get([
+                'private_videos.payment_id',
+                'private_videos.user_id',
+                'private_videos.created_at',
+                'video_uploads.*'
+            ]);
+
+        dd($owner_videos);
+
+        return view('test', ['owner_videos' => $owner_videos]);
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Bootstrap Example</title>
+    <title>Test Page</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
@@ -11,30 +11,47 @@
 <body>
 
     <div class="jumbotron text-center">
-        <h1>Test Page</h1>
+        <h1>Your Sell Transactions</h1>
     </div>
 
     <div class="container">
         <div class="row">
-            <ul>
-                @foreach ($obj as $r)
-                    <li>{{ $r->videoname }} <br><br>
-                        {{ str_replace('public', 'storage', $r->videoname) }}
-                        <h3>Image path: </h3>
-                        <video controls width="250">
-                            <source src={{ str_replace('public', 'storage', $r->videoname) }} type="video/webm">
-                            <source src={{ str_replace('public', 'storage', $r->videoname) }} type="video/mp4">
-                        </video>
-                        <p>Category: {{ $r->category->name }}</p>
-                        <p>Category: {{ $r->user_info->image }}</p>
 
-                        {{-- <img src="{{ str_replace('public', 'storage', $r->user_info->image) }}" width="100px"> --}}
-                    </li>
-                @endforeach
-            </ul>
-            <hr />
+            @foreach ($owner_videos as $item)
+                <ul>
+                    <li>Id: {{ $item->id }}</li>
+                    <li>Title: {{ $item->title }}</li>
+                    <li>Price: {{ $item->price }}</li>
+                    <li>Video Id: {{ $item->video_id }}</li>
+                </ul>
+            @endforeach
+
+            @php
+                $x = 0;
+
+                foreach ($owner_videos as $item) {
+                    $x = $x + $item->price;
+                }
+
+                $admin_commission = ($x * 19) / 100;
+
+                $after_split = $x - $admin_commission;
+
+            @endphp
 
         </div>
+
+        <h1>Total Earning : ${{ $x }}</h1>
+        <h1>Total Admin Commission : ${{ $admin_commission }}</h1>
+        <h1>After Split : ${{ $after_split }}</h1>
+
+
+        {{-- {{ $owner_videos->count() }} --}}
+
+        {{-- @foreach ($owner_videos->groupBy('video_id') as $item)
+                <dd>{{ $item[0] }}</dd>
+            @endforeach --}}
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
@@ -42,10 +59,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/bootstrap-tokenfield.js"></script>
 
-    <script>
-
-
-    </script>
+    <script></script>
 </body>
 
 </html>
