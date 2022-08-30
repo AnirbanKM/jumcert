cd973fa4bf8c24d530513f49cb6e640328772a2a
anirban <anirbanshow@kreativemachinez.net>
2022-08-30 12:17:48 +0530

    password reset fix

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -17,48 +17,54 @@ class PasswordResetController extends Controller
     {
         $email = $r->email;
 
-        // Check user's email exists or not
-        $user = User::where('email', '=', $r->email)->first();
+        try {
+            // Check user's email exists or not
+            $user = User::where('email', '=', $r->email)->first();
 
-        if ($user !== null) {
+            if ($user !== null) {
 
-            // Check if user email address already exists in passwordreset table
-            $checkEmail = PasswordReset::where('email', '=', $r->email)->get();
+                // Check if user email address already exists in passwordreset table
+                $checkEmail = PasswordReset::where('email', '=', $r->email)->get();
 
-            if (count($checkEmail) > 0) {
+                if (count($checkEmail) > 0) {
 
-                $html['msg'] = 'Password reset email already send, check your email';
-                $html['status'] = "success";
-                $html['code'] = 200;
-                echo json_encode($html);
-            } else {
-                // Generate Token
-                $token = Str::random(60);
+                    $html['msg'] = 'Password reset email already send, check your email';
+                    $html['status'] = "success";
+                    $html['code'] = 200;
+                    echo json_encode($html);
+                } else {
 
-                // Saving data to the password reset table
-                PasswordReset::create([
-                    'email' => $r->email,
-                    'token' => $token,
-                    'created_at' => Carbon::now()
-                ]);
+                    // Generate Token
+                    $token = Str::random(60);
 
-                // Sending Email with pasword reset view
-                Mail::send('email.reset_link', ['token' => $token], function (Message $message) use ($email) {
-                    $message->subject('Reset your password');
-                    $message->to($email);
-                });
-                $html['msg'] = 'Password reset email sent successfully, check your email';
-                $html['status'] = "success";
-                $html['code'] = 200;
-                echo json_encode($html);
+                    // Sending Email with pasword reset view
+                    $mailSend = Mail::send('email.reset_link', ['token' => $token], function (Message $message) use ($email) {
+                        $message->subject('Reset your password');
+                        $message->to($email);
+                    });
+
+                    // Saving data to the password reset table
+                    PasswordReset::create([
+                        'email' => $r->email,
+                        'token' => $token,
+                        'created_at' => Carbon::now()
+                    ]);
+
+                    $html['msg'] = 'Password reset email sent successfully, check your email';
+                    $html['status'] = "success";
+                    $html['code'] = 200;
+
+                    echo json_encode($html);
+                }
             }
-        } else {
-
-            return response()->json([
-                'msg' => 'Provided credentials are incorrect. Please try again',
-                'status' => 'error',
-            ]);
+        } catch (\Throwable $th) {
+            throw $th;
         }
+
+        // return response()->json([
+        //     'msg' => 'Provided credentials are incorrect. Please try again',
+        //     'status' => 'error',
+        // ]);
     }
 
     public function reset($token)
